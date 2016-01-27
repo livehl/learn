e5ca9deb6e2129e22171c2eef2c209e28d7fb375
livehl <livehl@126.com>
2016-01-27 11:10:16 +0800

    发布linux版本3

--- hello.go
+++ hello.go
@@ -4,7 +4,7 @@ import "net/http"
 import "fmt"
 
 func main() {
-	fmt.Println("Server start")                             //最简单的日志提示已经开始工作
-	http.Handle("/", http.FileServer(http.Dir("/public/"))) // 设置静态文件路由
-	http.ListenAndServe(":9100", nil)                       // 开始http server
+	fmt.Println("Server start")                            //最简单的日志提示已经开始工作
+	http.Handle("/", http.FileServer(http.Dir("public/"))) // 设置静态文件路由
+	http.ListenAndServe(":9100", nil)                      // 开始http server
 }
